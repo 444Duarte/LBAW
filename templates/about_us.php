721f4a94208eeaebb2de66f67599033f5069c76c
joaoDavidGB <joaobaiao12@gmail.com>
2016-03-07 15:35:42 +0000

    About Us

---
+++ templates/about_us.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+	<title>ToolBox User List</title>
+	<link href="../lib/bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/generic_template.css">
+</head>
+
+<body>
+	<?php include_once 'topbar.html';?>
+
+	<div class="container-fluid">
+	    <div class="row">
+	      	<?php include_once 'sidebar.html';?>
+
+
+	      	<div class="about-us">
+	      		<h1>About Us</h1>
+            	<p>A business big enough that it could be listed on the NASDAQ goes belly up. Disappears! It ceases to exist without me. No, you clearly don't know who you're talking to, so let me clue you in. I am not in danger, Skyler. I AM the danger! A guy opens his door and gets shot and you think that of me? No. I am the one who knocks! </p>
+
+				<p>Did he speak to you? Would you just answer? What things? What people? A month ago, Gus was trying to kill both of us. And now, he pulls you out of the lab and employs you as... what... a, an assistant gunman? A tough guy? Does that make any sense to you? He says he sees something in you. What kind of game is he playing. Does he think you're that naïve? He can't truly think that you'd forget... let alone Gale, let alone Victor... and all the horror that goes along with all of that. </p>
+
+				<p>It's enough. This is still the best way. You go after him with a gun, you'll never get out of it alive. But with this... you slip it into his food or drink, there shouldn't be any taste or smell... thirty-six hours later... poof. A man his age, working as hard as he does... no one will be surprised. Mike can have his suspicions, but that's all they'll be. Please, one homicidal maniac at a time. </p>
+          	</div>
+
+	   	</div>
+	</div>
+
+<script src="../lib/jquery-1.12.1.min.js"></script>
+<script src="../lib/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
+
+</body>
+</html>
