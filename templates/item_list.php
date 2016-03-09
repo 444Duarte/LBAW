bf8d93c226cbe0534652914c6e66052cf40bce09
444Duarte <duartemr.pinto@gmail.com>
2016-03-09 15:48:54 +0000

    Item List started. Missing categories

---
+++ templates/item_list.php
@@ -0,0 +1,66 @@
+<html>
+<?php include_once 'header.html';?>
+
+<body>
+	<?php include_once 'topbar.html';?>
+
+	<div class="container-fluid">
+	    <div class="row">
+	      	<?php include_once 'sidebar.html';?>
+	    	
+	    	<div class="col-sm-0 col-sm-offset-3 col-md-0 col-md-offset-2 main">
+	    		<ol class="breadcrumb">
+					  <li><a href="#">Home</a></li>
+					  <li><a href="#">Library</a></li>
+					  <li class="active">Data</li>
+					</ol>
+
+		    	<div class="col-xs-6 col-md-3">
+				    <a href="#" class="thumbnail">
+				      <img src="..." alt="Item Exemplo 1">
+				      <h3>Item Exemplo 1</h3>
+				    </a>
+				  </div>
+				  <div class="col-xs-6 col-md-3">
+				    <a href="#" class="thumbnail">
+				      <img src="..." alt="Item Exemplo 2">
+				      <h3>Item Exemplo 2</h3>
+				    </a>
+				  </div>
+				  <div class="col-xs-6 col-md-3">
+				    <a href="#" class="thumbnail">
+				      <img src="..." alt="Item Exemplo 3">
+				      <h3>Item Exemplo 3</h3>
+				    </a>
+				  </div>
+				  <div class="col-xs-6 col-md-3">
+				    <a href="#" class="thumbnail">
+				      <img src="..." alt="Item Exemplo 4">
+				      <h3>Item Exemplo 4</h3>
+				    </a>
+				  </div>
+				  <div class="col-xs-6 col-md-3">
+				    <a href="#" class="thumbnail">
+				      <img src="..." alt="Item Exemplo 5">
+				      <h3>Item Exemplo 5</h3>
+				    </a>
+				  </div>
+				  <div class="col-xs-6 col-md-3">
+				    <a href="#" class="thumbnail">
+				      <img src="..." alt="Item Exemplo 6">
+				      <h3>Item Exemplo 6</h3>
+				    </a>
+				  </div>
+	    	</div>
+
+	    </div>
+	</div>
+	
+<script src="../lib/jquery-1.12.1.min.js"></script>
+<script src="../lib/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+  $(".username").hide();
+  $("#loginButton");
+</script>
+</body>
+</html>
