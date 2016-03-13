2efdde583091a956dba956410066a157cf9af9a9
sceptross <up201304205@fe.up.pt>
2016-03-13 17:09:59 +0000

    All user stories done

--- templates/item.php
+++ templates/item.php
@@ -36,7 +36,6 @@
 							<li role="presentation" class="active"><a href="#info" aria-controls="info" role="tab" data-toggle="tab">Info</a></li>
 							<li role="presentation"><a href="#history" aria-controls="history" role="tab" data-toggle="tab">History</a></li>
 							<li role="presentation"><a href="#instances" aria-controls="instances" role="tab" data-toggle="tab">Instances</a></li>
-							<li role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Settings</a></li>
 						</ul>
 					</div>
 
@@ -77,13 +76,74 @@
 									</div>
 								</div>
 							</div>
-							<div role="tabpanel" class="tab-pane" id="history">Item History</div>
+							<div role="tabpanel" class="tab-pane" id="history">
+								<table class="table table-striped">
+					              <thead>
+					                  <tr>
+										<th>ID</th>
+									    <th>Action</th>
+					                    <th>Date</th>
+										<th>Manager</th>
+										<th>Client</th>
+					                  </tr>
+					              </thead>
+								  <tbody>
+					                <tr>
+									  <td>000003</td>
+					                  <td>Maintenance</td>
+					                  <td>14/03/2016</td>
+					                  <td>Pedro Ferreira</td>
+									  <td>MarMan - Manutencao de Martelos, Lda</td>
+					                </tr>
+									<tr>
+									  <td>000003</td>
+					                  <td>Return</td>
+					                  <td>14/03/2016</td>
+									  <td>Pedro Ferreira</td>
+									  <td><a href="user_profile.php">Marcelo Rebelo de Sousa</a></td>
+					                </tr>
+					                <tr>
+									  <td>000003</td>
+					                  <td>Lend</td>
+					                  <td>12/03/2016</td>
+									  <td>Afonso Castro</td>
+									  <td><a href="user_profile.php">Marcelo Rebelo de Sousa</a></td>
+					                </tr>
+									<tr>
+									  <td>000005</td>
+					                  <td>Add Instance</td>
+					                  <td>12/03/2016</td>
+									  <td>Afonso Castro</td>
+									  <td>---</td>
+					                </tr>    
+									<tr>
+									  <td>000003</td>
+					                  <td>Add Instance</td>
+					                  <td>12/03/2016</td>
+									  <td>Afonso Castro</td>
+									  <td>---</td>
+					                </tr>    
+					                <tr>
+									  <td>000001</td>
+					                  <td>Add Instance</td>
+					                  <td>12/03/2016</td>
+									  <td>Afonso Castro</td>
+									  <td>---</td>
+					                </tr>               
+					              </tbody>
+					            </table>
+							</div>
 							<div role="tabpanel" class="tab-pane" id="instances">
 								<div class="row">
 									<div class="col-sm-12">
 										<div class="panel panel-default">
-											<div class="panel-heading">
-													<h3 class="panel-title">Available Items</h3>
+											<div class="panel-heading clearfix">
+													<span class="panel-title">Available Items</span>
+													<span class="pull-right">
+													<button id="add-item-button" type="button" class="btn btn-default btn-lg" data-toggle="modal" data-target="#add-instance-modal" data-whatever="@getbootstrap">           
+														<span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add Instance
+													</button>
+													</span>
 												</div>
 												<div class="panel-body">
 													<table class="table table-striped">
@@ -101,6 +161,7 @@
 															<a href="#" title="Lend"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span></a>
 															<a href="#" title="Maintenance"><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span></a>
 															<a href="#" title="Return"><span class="glyphicon glyphicon-log-in transparent" aria-hidden="true"></span></a>
+															<a href="#" title="Remove"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a>
 															</td>
 														</tr>
 														<tr>
@@ -111,6 +172,7 @@
 															<a href="#" title="Lend"><span class="glyphicon glyphicon-log-out transparent" aria-hidden="true"></span></a>
 															<a href="#" title="Maintenance"><span class="glyphicon glyphicon-wrench transparent" aria-hidden="true"></span></a>
 															<a href="#" title="Return"><span class="glyphicon glyphicon-log-in" aria-hidden="true"></span></a>
+															<a href="#" title="Remove"><span class="glyphicon glyphicon-remove transparent" aria-hidden="true"></span></a>
 															</td>
 														</tr>
 														<tr>
@@ -121,6 +183,7 @@
 															<a href="#" title="Lend"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span></a>
 															<a href="#" title="Maintenance"><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span></a>
 															<a href="#" title="Return"><span class="glyphicon glyphicon-log-in transparent" aria-hidden="true"></span></a>
+															<a href="#" title="Remove"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a>
 															</td>
 														</tr>
 													</table>
@@ -137,6 +200,52 @@
 			</div>
 		</div>
 	</div>
+	
+	<div id="remove-instance-modal" class="modal fade" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              	<span aria-hidden="true">&times;</span>
+              </button>
+              <h4 class="modal-title">Remove Instance</h4>
+            </div> 
+            <div class="modal-body">
+              <form id="remove-instance-form" action="" method="post">
+                Condition: <?php print_stars(0)?>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="button" id="submit_form_button" class="btn btn-primary">Remove Instance</button>
+            </div>
+          </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+      </div><!-- /.modal -->
+	
+	<div id="add-instance-modal" class="modal fade" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              	<span aria-hidden="true">&times;</span>
+              </button>
+              <h4 class="modal-title">Add Instance</h4>
+            </div> 
+            <div class="modal-body">
+              <form id="add-instance-form" action="" method="post">
+                Condition: <?php print_stars(0)?>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="button" id="submit_form_button" class="btn btn-primary">Add Instance</button>
+            </div>
+          </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+      </div><!-- /.modal -->
+	  
+	  
 	<script src="../lib/jquery-1.12.1.min.js"></script>
 	<script src="../lib/bootstrap-3.3.6-dist/js/bootstrap.min.js"></script>
 	<script src="../lib/moment.min.js"></script>
