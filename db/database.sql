16f18cfb9ace2100d7a31df75660b31ac3b6afe0
444Duarte <duartemr.pinto@gmail.com>
2016-04-08 15:57:43 +0100

    OCD

--- db/database.sql
+++ db/database.sql
@@ -3,9 +3,9 @@ CREATE TYPE CONDITION AS ENUM
 	('1','2','3','4','5');
 	
 CREATE TABLE User(
-	id	INTEGER NOT NULL,
+	id	INTEGER 		NOT NULL,
 	username 	TEXT 	UNIQUE	NOT NULL,
-	email 	TEXT 	UNIQUE 	NOT NULL,
+	email 	TEXT 		UNIQUE 	NOT NULL,
 	password 	TEXT 	NOT NULL,
 	blocked BOOLEAN		DEFAULT FALSE,
 
@@ -14,10 +14,10 @@ CREATE TABLE User(
 );
 
 CREATE TABLE Client(
-	id INTEGER NOT NULL,
-	idCard INTEGER UNIQUE NOT NULL,
-	address TEXT NOT NULL,
-	phoneNumber TEXT NOT NULL,
+	id INTEGER 			NOT NULL,
+	idCard INTEGER 		UNIQUE NOT NULL,
+	address TEXT 		NOT NULL,
+	phoneNumber TEXT 	NOT NULL,
 	picture 	TEXT,
 	
 	PRIMARY KEY (id),
@@ -25,7 +25,7 @@ CREATE TABLE Client(
 );
 
 CREATE TABLE InventoryManager(
-	id 	INTEGER NOT NULL,
+	id 	INTEGER 		NOT NULL,
 	
 	PRIMARY KEY (id),
 	FOREIGN KEY (id) 	REFERENCES User(id)	
@@ -47,9 +47,9 @@ CREATE TABLE Category(
 );
 
 CREATE TABLE SubCategory(
-	id 	INTEGER NOT NULL,
-	name 	TEXT 	NOT NULL,
-	idCategory	INTEGER 	NOT NULL,
+	id 	INTEGER 				NOT NULL,
+	name 	TEXT 				NOT NULL,
+	idCategory	INTEGER 		NOT NULL,
 	
 	PRIMARY KEY (id),
 	FOREIGN KEY (idCategory) 	REFERENCES Category(id),
@@ -57,10 +57,10 @@ CREATE TABLE SubCategory(
 );
 
 CREATE TABLE Item(
-	id 	INTEGER NOT NULL,
-	name 	TEXT 	NOT NULL,
-	idSubCategory INTEGER	NOT NULL,
-	description TEXT NOT NULL,
+	id 	INTEGER 				NOT NULL,
+	name 	TEXT 				NOT NULL,
+	idSubCategory INTEGER		NOT NULL,
+	description TEXT 			NOT NULL,
 	picture 	TEXT,
 	
 	PRIMARY KEY (id),
@@ -68,18 +68,18 @@ CREATE TABLE Item(
 );
 
 CREATE TABLE ItemInstance(
-	id 	INTEGER NOT NULL,
-	idItem INTEGER	NOT NULL,
-	condition 	CONDITION NOT NULL,
+	id 	INTEGER 			NOT NULL,
+	idItem INTEGER			NOT NULL,
+	condition 	CONDITION 	NOT NULL,
 	
 	PRIMARY KEY (id),
 	FOREIGN KEY (idItem)	REFERENCES Item(id)
 );
 
 CREATE TABLE Reservation(
-	id 	INTEGER NOT NULL,
-	start 	DATE 	NOT NULL,
-	end 	DATE 	NOT NULL,
+	id 	INTEGER 			NOT NULL,
+	start 	DATE 			NOT NULL,
+	end 	DATE 			NOT NULL,
 	idClient	INTEGER 	NOT NULL,
 	idItemInstance 	INTEGER	NOT NULL,
 	fulfilled 	BOOLEAN 	DEFAULT FALSE,
@@ -90,52 +90,52 @@ CREATE TABLE Reservation(
 );
 
 CREATE TABLE ItemHistoryRecord(
-	id 	INTEGER NOT NULL,
-	date 	DATE 	NOT NULL,
-	idClient	INTEGER 	NOT NULL,
-	idItem 	INTEGER	NOT NULL,
+	id 	INTEGER 				NOT NULL,
+	date 	DATE 				NOT NULL,
+	idClient	INTEGER 		NOT NULL,
+	idItem 	INTEGER				NOT NULL,
 	idInventoryManager 	INTEGER	NOT NULL,
 	
 	PRIMARY KEY (id),
-	FOREIGN KEY (idClient)	REFERENCES Client(id),
-	FOREIGN KEY (idItem)	REFERENCES ItemInstance(id),
+	FOREIGN KEY (idClient)				REFERENCES Client(id),
+	FOREIGN KEY (idItem)				REFERENCES ItemInstance(id),
 	FOREIGN KEY (idInventoryManager)	REFERENCES InventoryManager(id)
 );
 
 CREATE TABLE AddRecord(
-	id 	INTEGER NOT NULL,
+	id 	INTEGER 		NOT NULL,
 	
 	PRIMARY KEY (id),
 	FOREIGN KEY (id)	REFERENCES ItemHistoryRecord(id),
 );
 
 CREATE TABLE RemoveRecord(
-	id 	INTEGER NOT NULL,
+	id 	INTEGER 		NOT NULL,
 	
 	PRIMARY KEY (id),
 	FOREIGN KEY (id)	REFERENCES ItemHistoryRecord(id),
 );
 
 CREATE TABLE LendRecord(
-	id 	INTEGER NOT NULL,
+	id 	INTEGER 			NOT NULL,
 	idClient	INTEGER 	NOT NULL,
 	
 	PRIMARY KEY (id),
-	FOREIGN KEY (id)	REFERENCES ItemHistoryRecord(id),
+	FOREIGN KEY (id)		REFERENCES ItemHistoryRecord(id),
 	FOREIGN KEY (idClient)	REFERENCES Client(id)
 );
 
 CREATE TABLE ReturnRecord(
-	id 	INTEGER NOT NULL,
-	idClient 	INTEGER	NOT NULL,
+	id 	INTEGER 			NOT NULL,
+	idClient 	INTEGER		NOT NULL,
 	
 	PRIMARY KEY (id),
-	FOREIGN KEY (id)	REFERENCES ItemHistoryRecord(id),
+	FOREIGN KEY (id)		REFERENCES ItemHistoryRecord(id),
 	FOREIGN KEY (idClient)	REFERENCES Client(id)
 );
 
 CREATE TABLE Maintenance(
-	id 	INTEGER NOT NULL,
+	id 	INTEGER 		NOT NULL,
 	repairer	TEXT	NOT NULL,
 	
 	PRIMARY KEY (id),
@@ -143,7 +143,7 @@ CREATE TABLE Maintenance(
 );
 
 CREATE TABLE Repaired(
-	id 	INTEGER NOT NULL,
+	id 	INTEGER 		NOT NULL,
 	
 	PRIMARY KEY (id),
 	FOREIGN KEY (id)	REFERENCES ItemHistoryRecord(id),
