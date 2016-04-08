1c0007f686f7ccb104060f54fd4e3079d925b6c2
444Duarte <duartemr.pinto@gmail.com>
2016-04-08 17:58:46 +0100

    Physical model

--- db/database.sql
+++ db/database.sql
@@ -1,16 +1,30 @@
 
 CREATE TYPE CONDITION AS ENUM
 	('1','2','3','4','5');
+CREATE TYPE RECORD_TYPE AS ENUM
+	('Add','Remove','Lend','Return','Maintenance','Repaired');
+CREATE TYPE USER_TYPE AS ENUM
+	('Client','InventoryManager', 'SystemManager');
+
 	
 CREATE TABLE User(
-	id	INTEGER 		NOT NULL,
-	username 	TEXT 	UNIQUE	NOT NULL,
-	email 	TEXT 		UNIQUE 	NOT NULL,
-	password 	TEXT 	NOT NULL,
-	blocked BOOLEAN		DEFAULT FALSE,
+	id			INTEGER 	NOT NULL,
+	username 	TEXT 		UNIQUE	NOT NULL,
+	email 		TEXT 		UNIQUE 	NOT NULL,
+	password 	TEXT 		NOT NULL,
+	type 		USER_TYPE 	NOT NULL,
 
-	PRIMARY KEY (id)
-	--TODO ADD CONSTRAINTS
+	PRIMARY KEY (id),
+	CHECK (LEN(password) >6)
+);
+
+CREATE TABLE UserBlocked(
+	id 		INTEGER 	NOT NULL,
+	date 	TIMESTAMP 	NOT NULL DEFAULT NOW,
+	note 	TEXT,
+
+	PRIMARY KEY (id),
+	FOREIGN KEY (id) 	REFERENCES User(id)
 );
 
 CREATE TABLE Client(
@@ -21,23 +35,9 @@ CREATE TABLE Client(
 	picture 	TEXT,
 	
 	PRIMARY KEY (id),
-	FOREIGN KEY (id) 	REFERENCES User(id)
+	FOREIGN KEY (id) 	REFERENCES User(id),
+	CHECK (LEN(phoneNumber) = 9)
 );
-
-CREATE TABLE InventoryManager(
-	id 	INTEGER 		NOT NULL,
-	
-	PRIMARY KEY (id),
-	FOREIGN KEY (id) 	REFERENCES User(id)	
-);
-
-CREATE TABLE SystemManager(
-	id 	INTEGER NOT NULL,
-	
-	PRIMARY KEY (id),
-	FOREIGN KEY (id) 	REFERENCES User(id)	
-);
-
 
 CREATE TABLE Category(
 	id 	INTEGER NOT NULL,
@@ -47,8 +47,8 @@ CREATE TABLE Category(
 );
 
 CREATE TABLE SubCategory(
-	id 	INTEGER 				NOT NULL,
-	name 	TEXT 				NOT NULL,
+	id 			INTEGER 		NOT NULL,
+	name 		TEXT			NOT NULL,
 	idCategory	INTEGER 		NOT NULL,
 	
 	PRIMARY KEY (id),
@@ -64,7 +64,8 @@ CREATE TABLE Item(
 	picture 	TEXT,
 	
 	PRIMARY KEY (id),
-	FOREIGN KEY (idSubCategory)	REFERENCES SubCategory(id)
+	FOREIGN KEY (idSubCategory)	REFERENCES SubCategory(id),
+	CHECK (LEN(description) > 20)
 );
 
 CREATE TABLE ItemInstance(
@@ -77,43 +78,31 @@ CREATE TABLE ItemInstance(
 );
 
 CREATE TABLE Reservation(
-	id 	INTEGER 			NOT NULL,
-	start 	DATE 			NOT NULL,
-	end 	DATE 			NOT NULL,
-	idClient	INTEGER 	NOT NULL,
-	idItemInstance 	INTEGER	NOT NULL,
-	fulfilled 	BOOLEAN 	DEFAULT FALSE,
+	id 				INTEGER 	NOT NULL,
+	start 			TIMESTAMP 	NOT NULL,
+	end 			TIMESTAMP 	NOT NULL,
+	idClient		INTEGER 	NOT NULL,
+	idItemInstance 	INTEGER		NOT NULL,
+	fulfilled 		BOOLEAN 	DEFAULT FALSE,
 	
 	PRIMARY KEY (id),
 	FOREIGN KEY (idClient)	REFERENCES Client(id),
-	FOREIGN KEY (idItem)	REFERENCES ItemInstance(id)
+	FOREIGN KEY (idItem)	REFERENCES ItemInstance(id),
+	CHECK(end > start)
 );
 
 CREATE TABLE ItemHistoryRecord(
-	id 	INTEGER 				NOT NULL,
-	date 	DATE 				NOT NULL,
-	idClient	INTEGER 		NOT NULL,
-	idItem 	INTEGER				NOT NULL,
-	idInventoryManager 	INTEGER	NOT NULL,
+	id 					INTEGER 	NOT NULL,
+	date 				TIMESTAMP 	NOT NULL DEFAULT NOW,
+	idClient			INTEGER 	NOT NULL,
+	idItem 				INTEGER		NOT NULL,
+	idInventoryManager 	INTEGER		NOT NULL,
+	type 				RECORD_TYPE	NOT NULL,	
 	
 	PRIMARY KEY (id),
 	FOREIGN KEY (idClient)				REFERENCES Client(id),
 	FOREIGN KEY (idItem)				REFERENCES ItemInstance(id),
 	FOREIGN KEY (idInventoryManager)	REFERENCES InventoryManager(id)
-);
-
-CREATE TABLE AddRecord(
-	id 	INTEGER 		NOT NULL,
-	
-	PRIMARY KEY (id),
-	FOREIGN KEY (id)	REFERENCES ItemHistoryRecord(id),
-);
-
-CREATE TABLE RemoveRecord(
-	id 	INTEGER 		NOT NULL,
-	
-	PRIMARY KEY (id),
-	FOREIGN KEY (id)	REFERENCES ItemHistoryRecord(id),
 );
 
 CREATE TABLE LendRecord(
@@ -137,17 +126,12 @@ CREATE TABLE ReturnRecord(
 CREATE TABLE Maintenance(
 	id 	INTEGER 		NOT NULL,
 	repairer	TEXT	NOT NULL,
+	expectedEnd DATE 	NOT NULL,
 	
 	PRIMARY KEY (id),
 	FOREIGN KEY (id)	REFERENCES ItemHistoryRecord(id),
 );
 
-CREATE TABLE Repaired(
-	id 	INTEGER 		NOT NULL,
-	
-	PRIMARY KEY (id),
-	FOREIGN KEY (id)	REFERENCES ItemHistoryRecord(id),
-);
 
 
 
