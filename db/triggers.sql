be389427c9939261e04eb0447ab7bc4b0e429e02
444Duarte <duartemr.pinto@gmail.com>
2016-04-10 20:49:20 +0100

    Trigger-client mismatch

---
+++ db/triggers.sql
@@ -0,0 +1,69 @@
+CREATE TRIGGER addRecord
+BEFORE INSERT ON ItemHistoryRecord
+FOR EACH ROW 
+WHEN
+	(
+		NEW.type = 'Add'
+		AND
+		(	SELECT type, MAX(date) 						--VER SE NÃO EXISTE NENHUM REGISTO ANTERIOR ANTES DE ADICIONAR UM ADD RECORD
+			FROM ItemHistoryRecord
+			WHERE idItemInstance = NEW.idItemInstance
+		)>0
+		 
+	)
+	OR
+	(
+		(NEW.type = 'Lend' OR NEW.type = 'Maintenace')
+		AND 
+		(
+			SELECT *
+			FROM(SELECT type, MAX(date) 						--
+				FROM ItemHistoryRecord
+				WHERE idItemInstance = NEW.idItemInstance)
+			WHERE type = 'Add' OR type = 'Return' OR type ='Repaired'
+		) = 0	
+	)
+	OR
+	(
+		NEW.type = 'Return'
+		AND 
+		(
+			SELECT *
+			FROM(SELECT type, MAX(date) 						--
+				FROM ItemHistoryRecord
+				WHERE idItemInstance = NEW.idItemInstance)
+			WHERE type = 'Lend'
+		) = 0
+	)
+	OR
+	(
+		NEW.type = 'Repaired'
+		AND 
+		(
+			SELECT *
+			FROM(SELECT type, MAX(date) 						--
+				FROM ItemHistoryRecord
+				WHERE idItemInstance = NEW.idItemInstance)
+			WHERE type = 'Maintenace'
+		) = 0
+	)
+BEGIN
+	SELECT RAISE (ABORT, "Can't perform this action");
+END
+;
+
+CREATE TRIGGER clientMismatch
+BEFORE INSERT ON ReturnRecord
+FOR EACH ROW 
+WHEN
+	(SELECT *
+	FROM(SELECT *, MAX(date) 						--
+		FROM ItemHistoryRecord
+		NATURAL JOIN LendRecord
+		WHERE idItemInstance = NEW.idItemInstance)
+	WHERE idClient = NEW.idClient	
+	) <= 0
+BEGIN
+	SELECT RAISE (ABORT, "Client mismatch on ReturnRecord");
+END
+;	
