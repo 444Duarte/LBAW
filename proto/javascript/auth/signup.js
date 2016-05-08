2b55e382aeeb4dd3d5e8faaba86b548793333cfa
444Duarte <duartemr.pinto@gmail.com>
2016-05-09 00:39:02 +0100

    check passwords

--- proto/javascript/auth/signup.js
+++ proto/javascript/auth/signup.js
@@ -1,10 +1,22 @@
 $("document").ready(function(){
 	$('#username').focusout(validUsername);
-
+	$('#signup_form input[type="submit"]').click(checkPasswords);
 
 };
 
-function validUsername() {
+function checkPasswords(e){
+	var pass = $('#signup_form input[name="password"]').val();  
+	var confirmPassword = $('#signup_form input[name="confirm_password"]').val();
+	if(pass != confirmPassword){
+		$('#username').css('border-color', 'solid 1px red');
+		alert("Passowords don't match");
+		e.preventDefault();
+	}else{
+		return true;
+	}
+};
+
+function validUsername(e) {
 	var input = $('#username').val();
 		
 	$.post("api/auth/validate_username.php",
@@ -17,4 +29,4 @@ function validUsername() {
 		}
  	},
  	'json');
-}
+};
