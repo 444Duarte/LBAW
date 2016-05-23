86021653c9b45933d18f0438eff82a933ad731aa
sceptross <up201304205@fe.up.pt>
2016-05-23 13:23:46 +0100

    Solve merge conflicts

---
+++ proto/config/init.php
@@ -0,0 +1,40 @@
+<?php
+  session_set_cookie_params(3600, '/~up201304205');
+  session_start();
+
+  error_reporting(E_ALL); 
+
+  
+  $BASE_DIR = '/usr/users2/mieic2013/up201304205/public_html/lbaw/proto/'; 
+  $BASE_URL = '/~up201304205/lbaw/proto/'; 
+
+  $conn = new PDO('pgsql:host=dbm;dbname=lbaw1533', 'lbaw1533', 'VN12B1N7'); 
+  $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $conn->exec('SET SCHEMA \'proto\''); 
+
+  include_once($BASE_DIR . 'lib/smarty/Smarty.class.php');
+  
+  $smarty = new Smarty;
+  $smarty->template_dir = $BASE_DIR . 'templates/';
+  $smarty->compile_dir = $BASE_DIR . 'templates_c/';
+  $smarty->assign('BASE_URL', $BASE_URL);
+
+  $existing = $smarty->getPluginsDir();
+  $existing[] = getcwd() . '/plugins';
+  $smarty->setPluginsDir($existing);
+
+  $username = isset($_SESSION['username']) ? $_SESSION['username'] : NULL;
+  
+  $smarty->assign('ERROR_MESSAGES', isset($_SESSION['error_messages']) ? $_SESSION['error_messages'] : NULL);  
+  $smarty->assign('FIELD_ERRORS', isset($_SESSION['field_errors']) ? $_SESSION['field_errors'] : NULL);
+  $smarty->assign('SUCCESS_MESSAGES', isset($_SESSION['success_messages']) ? $_SESSION['success_messages'] : NULL);
+  $smarty->assign('FORM_VALUES', isset($_SESSION['form_values']) ? $_SESSION['form_values'] : NULL);
+  $smarty->assign('USERNAME', $username);
+  
+  unset($_SESSION['success_messages']);
+  unset($_SESSION['error_messages']);  
+  unset($_SESSION['field_errors']);
+  unset($_SESSION['form_values']);
+?>
