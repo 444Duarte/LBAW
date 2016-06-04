c930f027c6d9d96f73b11f6aa87d574d213e1cc3
joaoDavidGB <joaobaiao12@gmail.com>
2016-06-04 16:45:47 +0100

    fix

--- proto/actions/user/book_item.php
+++ proto/actions/user/book_item.php
@@ -1,29 +1,27 @@
 <?php 
 	include_once('../../config/init.php');
 	include_once($BASE_DIR .'database/users.php');
+	include_once($BASE_DIR .'database/util.php');
 
 
-	if(!isset($_POST['item_instance']) || !isset($_POST['start_date']) || !isset($_POST['end_date'])) {
+	if(!isset($_POST['item_instance']) || !isset($_POST['start_date']) || !isset($_POST['end_date']) || $_POST['end_date'] == "" || $_POST['start_date'] == "" || $_POST['item_instance'] == "") {
 		$_SESSION['error_messages'][] = 'Invalid Input';
 		$_SESSION['form_values'] = $_POST;
-		//echo 'cenas';
-		//echo $_POST['id_client'];
-		echo $_POST['item_instance'];
-		echo $_POST['start_date'];
-		echo $_POST['end_date'];
-		//header('Location: ' . $_SERVER['HTTP_REFERER']);
+		
+		header('Location: ' . $_SERVER['HTTP_REFERER']);
 		exit;
 	}
 
 
 	if ($username == NULL){
 		echo 'User not logged';
+		header('Location: ' . $_SERVER['HTTP_REFERER']);
 		exit;
 	}
 
 	$idClient = getUserByUsername($username)['id'];
 
-	$item = $_POST['item_instance'];
+	$item = test_input($_POST['item_instance']);
 	$start = $_POST['start_date'];
 	$end = $_POST['end_date'];
 
